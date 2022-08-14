bb1d8b9a05ef79c3efbaad946e245989b99a778d
Myron <shikhovmyron@gmail.com>
2022-08-14 19:53:52 +0200

    add access

--- tasks-api/tasks-app.js
+++ tasks-api/tasks-app.js
@@ -11,6 +11,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next();
+})
 const extractAndVerifyToken = async (headers) => {
   if (!headers.authorization) {
     throw new Error('No token provided.');
